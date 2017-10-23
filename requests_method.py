a1e03f723c62db9cbfde837890d57fe2498b67bc
start712 <start712@protonmail.com>
2017-10-23 08:50:45 +0800

    1023 0850

--- requests_method.py
+++ requests_method.py
@@ -16,13 +16,13 @@ sys.path.append(sys.prefix + "\\Lib\\MyWheels")
 reload(sys)
 sys.setdefaultencoding('utf8')
 import requests_manager
+import func_manager
 requests_manager = requests_manager.requests_manager()
-
-url_func = lambda url, s, s0, b:url + s.replace(s0,'') if b else s
+func_manager = func_manager.func_manager()
 
 class requests_method(object):
     def __init__(self):
-        pass
+        self.url_func = lambda s, url, s0:url + s.replace(s0,'')
 
     def catalog_requests_1(self, item, url, args):
         print "准备分析目录页: ", url
@@ -56,11 +56,29 @@ class requests_method(object):
         e_table = bs_obj.find_all(args["e_table"]["tag"], attrs=args["e_table"]["attrs"])
         job_title = ''
         for e_table0 in e_table:
-            for e in e_table0.childGenerator():
+            for e in e_table0.find_all(args['catalog_mehod']['tag']):
                 if isinstance(e, bs4.NavigableString):
                     continue
-                if e.name == "p": # 这里职称单独写在一行
-                     job_title = e.get_text(strip=True)
+                for title in args['catalog_mehod']['data']:
+                    print title
+                    e_method = args['catalog_mehod']['data'][title]
+                    print (e, e_method["func"], e_method["args"])
+                    res = func_manager.run_partial_func(e, e_method["func"], e_method["args"])
+                    if res is None:
+                        res = getattr(self, e_method["func"])(**e_method["args"])
+                    print "res:",res
+                    if 'more_func' in e_method:
+                        for s in e_method['more_func']['func']:
+                            arg_list = e_method['more_func']['args'][s]
+                            func = getattr(self, s)
+                            l = [res,]
+                            l.extend(arg_list)
+                            res = func(*l)
+
+                    item[title] = res
+                yield item
+
+                """
                 elif e.name == 'table':
                     e_as = e.find_all('a')
                     for e_a in e_as:
@@ -71,6 +89,7 @@ class requests_method(object):
                         item["课程"] = e_a.find_next('td').get_text(strip=True)
 
                         yield item
+                """
 
     def detail_requests_1(self, item, url, args):
         print "准备分析详情页页: ", url
